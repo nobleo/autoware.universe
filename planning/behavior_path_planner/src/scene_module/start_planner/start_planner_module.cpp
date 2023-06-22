fa0ab239d63528155cd8188d0b05eb180f9566a7
kosuke55 <kosuke.tnp@gmail.com>
2023-06-22 20:30:43 +0900

    fix(start_planner): not run other modules when back pull out
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- planning/behavior_path_planner/src/scene_module/start_planner/start_planner_module.cpp
+++ planning/behavior_path_planner/src/scene_module/start_planner/start_planner_module.cpp
@@ -270,6 +270,8 @@ BehaviorModuleOutput StartPlannerModule::plan()
   });
 
   if (status_.back_finished) {
+    setIsSimultaneousExecutableAsApprovedModule(true);
+    setIsSimultaneousExecutableAsCandidateModule(true);
     const double start_distance = motion_utils::calcSignedArcLength(
       path.points, planner_data_->self_odometry->pose.pose.position,
       status_.pull_out_path.start_pose.position);
@@ -283,6 +285,8 @@ BehaviorModuleOutput StartPlannerModule::plan()
       {start_distance, finish_distance}, SteeringFactor::START_PLANNER, steering_factor_direction,
       SteeringFactor::TURNING, "");
   } else {
+    setIsSimultaneousExecutableAsApprovedModule(false);
+    setIsSimultaneousExecutableAsCandidateModule(false);
     const double distance = motion_utils::calcSignedArcLength(
       path.points, planner_data_->self_odometry->pose.pose.position,
       status_.pull_out_path.start_pose.position);
@@ -394,6 +398,8 @@ BehaviorModuleOutput StartPlannerModule::planWaitingApproval()
   });
 
   if (status_.back_finished) {
+    setIsSimultaneousExecutableAsApprovedModule(true);
+    setIsSimultaneousExecutableAsCandidateModule(true);
     const double start_distance = motion_utils::calcSignedArcLength(
       stop_path.points, planner_data_->self_odometry->pose.pose.position,
       status_.pull_out_path.start_pose.position);
@@ -406,6 +412,8 @@ BehaviorModuleOutput StartPlannerModule::planWaitingApproval()
       {start_distance, finish_distance}, SteeringFactor::START_PLANNER, steering_factor_direction,
       SteeringFactor::APPROACHING, "");
   } else {
+    setIsSimultaneousExecutableAsApprovedModule(false);
+    setIsSimultaneousExecutableAsCandidateModule(false);
     const double distance = motion_utils::calcSignedArcLength(
       stop_path.points, planner_data_->self_odometry->pose.pose.position,
       status_.pull_out_path.start_pose.position);
